8522aec49a215e147db16ec20403f9ce37cfe06b
LFelipeAravena <luisf.aravenarojas@gmail.com>
2021-03-03 16:13:27 -0300

    Curso Ionic hasta clase 76

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Componente } from '../interfaces/interfaces';
+import { delay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +14,20 @@ export class DataService {
   getUsuarios(){ //Un observable
     return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
+
+  getAlbumes(){ //Un observable de search
+    return this.http.get<any[]>('https://jsonplaceholder.typicode.com/albums');
+  }
   
   getMenuOpts(){ //Un observable
     return this.http.get<Componente[]>('/assets/data/menu-opts.json');
+  }
+
+  getHeroes(){ //Un observable
+    return this.http.get<Componente[]>('/assets/data/superheroes.json')
+               .pipe(
+                  delay(1500)
+               );
   }
 
 }
